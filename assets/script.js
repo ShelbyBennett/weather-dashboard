2da4bb9047c87a66128ca131fdedc06d60914931
Shelby Bennett <shelshel@Shelbys-MBP.lan>
2023-10-02 11:37:22 -0400

    created part of the html format, added some css and bootstrap, added the two fetches for weather and forecast

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,43 @@
-// get the API Key
-const apikey =
+const searchBtn= document.getElementById("search-btn")
+const searchInput= document.getElementById("input-search")
 
-document.getElementById('searchBtn').addEventListener('click', () =>)
+// get the API Key
+const apiKey ="d7104f28f24bae988a39232fd8b0d446"
+
+
+function getWeather(cityName){
+ const url =`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`
+fetch(url)
+.then((response)=>response.json())
+.then((data)=>{
+    console.log(data);
+    const lat =data.coord.lat
+    const lon=data.coord.lat
+    getForecast(lat, lon)
+})
+
+}
+
+function getForecast(lat, lon){
+    const url= `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
+    fetch(url)
+    .then((response)=>response.json())
+    .then((data)=>{
+        console.log(data);
+    })
+}
+
+
+
+
+
+
+
+
+searchBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    const city = searchInput.value;
+    console.log(city);
+    getWeather(city)
+   
+})
