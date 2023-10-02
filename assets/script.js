533f0dcb9f8fe80e5475a8063b9d6775df0bf6eb
Shelby Bennett <shelshel@Shelbys-MBP.lan>
2023-10-02 12:03:09 -0400

    worked on structuring and gettin temp, humidity anf wind speed to populate

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,10 @@
 const searchBtn= document.getElementById("search-btn")
 const searchInput= document.getElementById("input-search")
+const currentWeather= document.querySelector(".current-weather")
 
 // get the API Key
 const apiKey ="d7104f28f24bae988a39232fd8b0d446"
-
+let output;
 
 function getWeather(cityName){
  const url =`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`
@@ -14,6 +15,7 @@ fetch(url)
     const lat =data.coord.lat
     const lon=data.coord.lat
     getForecast(lat, lon)
+    displayWeather(data)
 })
 
 }
@@ -27,6 +29,22 @@ function getForecast(lat, lon){
     })
 }
 
+function displayWeather(data){
+
+     output+= `<div class="card" id="main-card">
+     <div class="card-header">
+      <h2 class="card-title">${data.name}<span></span></h2>
+     </div>
+     <div class="card-body">
+       <p class="card-text"> Temperature: ${data.main.temp} F</p>
+       <p class="card-text"> Humidity: ${data.main.humidity} %</p>
+       <p class="card-text"> Wind Speed: ${data.wind.speed} MPH</p>
+      
+     </div>
+   </div>`
+
+   currentWeather.innerHTML= output;
+}
 
 
 
