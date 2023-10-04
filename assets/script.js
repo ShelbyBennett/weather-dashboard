1e1d8777876a05381345492c56fa02dae2553293
Shelby Bennett <shelshel@Shelbys-MBP.lan>
2023-10-04 14:06:46 -0400

    working on forecast js

--- assets/script.js
+++ assets/script.js
@@ -56,9 +56,52 @@ function displayWeather(data){
    const cardHeader=document.createElement("div")
    cardHeader.setAttribute("class","card-header has-text-centered")
    const cardHeaderTitle= document.createElement("h2")
-   
+   cardHeaderTitle.setAttribute("class","card-title")
+   cardHeaderTitle.textContent=data.cityName
+   const span = document.createElement("span")
+   const icon = document.createElement("img")
+   icon.setAttribute("src","https://openweathermap.org/img/w/" + data.weather[0].icon + ".png" )
+
+   //card body
+   const cardBody=document.createElement("div")
+   cardBody.setAttribute("class", "card-body main-body")
+   const temp= document.createElement("p")
+      const humidity= document.createElement("p")
+      const wind =document.createElement("p")
+      const span1=document.createElement("span")
+      span1.setAttribute("class","labels")
+      const span2=document.createElement("span")
+      span2.setAttribute("class","labels")
+      const span3=document.createElement("span")
+      span3.setAttribute("class","labels")
+      span1.textContent="Temperature"
+      temp.textContent=`${data.main.temp} F`
+      span2.textContent="Humidity: "
+      humidity.textContent= `${data.main.humidity} %`
+      span3.textContent="Wind Speed: "
+      wind.textContent= `${data.wind.speed} MPH`
+
+      //ELEMENTS
+      span.append(icon)
+      cardHeaderTitle.append(span)
+      cardHeader.append(cardHeaderTitle)
+      temp.prepend(span1)
+      humidity.prepend(span2)
+      wind.prepend(span3)
+      cardBody.append(cardHeader,cardBody)
+      currentWeather.append(card)
 }
 
+function displayForecast(data){
+
+    forecastEL.innerHTML=""
+
+    for (let i= 0; i< 5; i++) {
+
+        const card= document.createElement("div")
+        
+    }
+}
 
 
 
